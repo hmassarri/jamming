2ee31468afb8ae1b8473d544e4c146683d2ebac1
Hugo M <hugo@hugomassarri.com>
2019-04-23 20:38:54 -0400

    Initial commit - Set up components and App.js - all css complete

---
+++ src/components/Playlist/Playlist.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './Playlist.css';
+import TrackList from '../TrackList/TrackList';
+
+
+class Playlist extends React.Component {
+  render() {
+    return (
+      <div className="Playlist">
+        <input value={'New Playlist'}/>
+
+        <a className="Playlist-save">SAVE TO SPOTIFY</a>
+      </div>
+    );
+  }
+}
+
+export default Playlist;
---
+++ src/components/SearchResults/SearchResults.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './SearchResults.css';
+
+import TrackList from '../TrackList/TrackList';
+
+class SearchResults extends React.Component {
+  render() {
+    return(
+      <div className="SearchResults">
+        <h2>Results</h2>
+        <TrackList />
+      </div>
+    );
+  }
+}
+
+export default SearchResults;
